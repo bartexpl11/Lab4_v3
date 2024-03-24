3f18be6bb5a9ea988715018aecdc2075e3992a07
bartexpl11 <175180@student.uwm.edu.pl>
2024-03-24 09:19:07 +0100

    zmiana

--- main.py
+++ main.py
@@ -26,3 +26,38 @@ print(macierz)
 print(macierz.shape)
 print(macierz.ndim)
 
+zera = np.zeros((5,5))
+jedynki = np.ones((4,4))
+print(zera)
+print(zera.dtype)
+print(jedynki)
+print(jedynki.dtype)
+
+pusta = np.empty((2,2))
+print(pusta)
+poz_1 = pusta[1,1]
+poz_2 = pusta[0,1]
+print(poz_1)
+print(poz_2)
+
+macierz = np.array([[1,2],[3,4]])
+print(macierz)
+liczby = np.arange(1, 2, 0.1)
+print(liczby)
+
+liczby_lin = np.linspace(1, 2, 5)
+print(liczby_lin)
+
+z = np.indices((5,3))
+print(z)
+print(z[1,3,1])
+
+x, y = np.mgrid[0:5, 0:5]
+print(x)
+print(y)
+
+mat_diag_k = np.diag([a for a in range(5)], k=1)
+print(mat_diag_k)
+
+z = np.fromiter(range(5), dtype='int32')
+print(z)
