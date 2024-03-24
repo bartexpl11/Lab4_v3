400ca24b3ebc32086e47de8e752209aaa2ee24bf
bartexpl11 <175180@student.uwm.edu.pl>
2024-03-24 08:38:41 +0100

    zmiana

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("hello")
-print("hello")
-print("hello")
+import numpy as np
+a = np.array([0, 1])
+print(a)
+
+a = np.arange(2)
+print(a)
+print(type(a))
+
+print(a.dtype)
+
+a = np.arange(2, dtype='int32')
+print(a.dtype)
+b = a.astype('float')
+print(b)
+print(b.dtype)
+print(b.shape)
+
+print(a.ndim)
+
+m=np.array((np.arange(2),np.arange(2)))
+print(m.shape)
+print(m.ndim)
+
